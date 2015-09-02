68ff690ddf48df0357d2d49ed4e348b53f7dfc94
Hubert <huberttesting@gmail.com>
2015-09-03 00:31:14 +0200

    Adds sport model with test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902221923) do
+ActiveRecord::Schema.define(version: 20150902222827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20150902221923) do
 
   create_table "players", force: :cascade do |t|
     t.string   "full_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
