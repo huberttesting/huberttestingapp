1698d6fbdb560b04899494e9d398c5369c0461ac
Hubert <huberttesting@gmail.com>
2015-09-06 18:27:30 +0200

    Adds tests fot customer and video models

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   it { expect(subject).to have_db_column(:name) }
   it { expect(subject).to have_and_belong_to_many(:videos) }
+  it { expect(subject).to validate_presence_of(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Video, type: :model do
   it { expect(subject).to have_db_column(:name) }
   it { expect(subject).to have_and_belong_to_many(:customers) }
+  it { expect(subject).to validate_presence_of(:customers) }
 end
